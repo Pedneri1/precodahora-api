94743e4bc9ce769b0e2d39c80326fcc1ecc671bc
Pedro Neri <pedneri1@gmail.com>
2020-05-08 19:44:54 -0300

    Add source, test and README files (#1)
    * Add source, test and README files
    
    * Remove constructor parameter, adds config file and XO linter

---
+++ src/index.js
@@ -0,0 +1,117 @@
+const { Cookie } = require("tough-cookie");
+const userAgentWithoutSeed = require("useragent-from-seed");
+const request = require("axios").default;
+const HTMLParser = require("node-html-parser");
+const querystring = require("querystring");
+
+const config = require("./config");
+
+class PrecoDaHora {
+  constructor() {
+    const userAgent = userAgentWithoutSeed();
+
+    const requestOptions = {};
+
+    requestOptions.baseURL = config.baseUrl;
+    requestOptions.headers = {
+      Accept: "*/*",
+      "Accept-Language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
+      Connection: "keep-alive",
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+      Origin: config.baseUrl,
+      Referer: config.baseUrl,
+      "Sec-Fetch-Dest": "empty",
+      "Sec-Fetch-Mode": "cors",
+      "Sec-Fetch-Site": "same-origin",
+      "User-Agent": userAgent,
+      "X-CSRFToken": "",
+      "X-Requested-With": "XMLHttpRequest",
+    };
+    requestOptions.xsrfCookieName = "X-CSRFToken";
+
+    this.requestOptions = requestOptions;
+    this.request = request.create(requestOptions);
+  }
+
+  async _getCookiesAndCsrfToken({ page }) {
+    const response = await this.request.get(page);
+
+    const responseDataParsed = HTMLParser.parse(response.data);
+
+    const cookies = response.headers["set-cookie"]
+      .map((cookie) => Cookie.parse(cookie))
+      .map((cookie) => `${cookie.key}=${cookie.value}; `);
+
+    return {
+      csrfToken: responseDataParsed
+        .querySelector("#validate")
+        .getAttribute("data-id"),
+      cookies: cookies.join(""),
+    };
+  }
+
+  async _setCookiesAndCsrfToken({ csrfToken, cookies }) {
+    this.request.defaults.headers["X-CSRFToken"] = csrfToken;
+    this.request.defaults.headers.Cookie = cookies;
+  }
+
+  async sugestao({ item }) {
+    this._setCookiesAndCsrfToken(
+      await this._getCookiesAndCsrfToken({ page: "/" })
+    );
+
+    return this.request.post("/sugestao/", `item=${item}`);
+  }
+
+  async produto({
+    termo = "",
+    gtin = "",
+    cnpj = "",
+    horas = "",
+    anp = "",
+    codmun = "",
+    latitude = "",
+    longitude = "",
+    raio = "",
+    precomax = 0,
+    precomin = 0,
+    pagina = "",
+    ordenar = "",
+    categorias = "",
+    processo = "",
+    totalCategorias = "",
+    totalRegistros = "",
+    totalPaginas = "",
+    pageview = "",
+  }) {
+    this._setCookiesAndCsrfToken(
+      await this._getCookiesAndCsrfToken({ page: "/produtos/" })
+    );
+
+    const parameters = querystring.stringify({
+      termo,
+      gtin,
+      cnpj,
+      horas,
+      anp,
+      codmun,
+      latitude,
+      longitude,
+      raio,
+      precomax,
+      precomin,
+      pagina,
+      ordenar,
+      categorias,
+      processo,
+      totalCategorias,
+      totalRegistros,
+      totalPaginas,
+      pageview,
+    });
+
+    return this.request.post("/produtos/", parameters);
+  }
+}
+
+module.exports = PrecoDaHora;
---
+++ test/index.js
@@ -0,0 +1,28 @@
+const test = require('ava');
+const PrecoDaHora = require('../src');
+
+const client = new PrecoDaHora();
+
+test('getSugestao', async t => {
+	const response = await client.sugestao({item: 'DIPIRONA'});
+	t.is(response.data.codigo, 80);
+});
+
+test('getProduto', async t => {
+	const response = await client.produto({
+		gtin: 7891055317303,
+		horas: 72,
+		latitude: -12.2733,
+		longitude: -38.9556,
+		raio: 15,
+		precomax: 0,
+		precomin: 0,
+		ordenar: 'preco.asc',
+		pagina: 1,
+		processo: 'carregar',
+		totalRegistros: 0,
+		totalPaginas: 0,
+		pageview: 'lista'
+	});
+	t.is(response.data.codigo, 80);
+});
